57157648f8c74a9627986f4f1eb0310aeb9266f2
Ankit Parkhi <ankitvilas.parkhi.cse23@itbhu.ac.in>
2026-01-05 18:50:25 +0530

    part7b completed

---
+++ part7/routed-anecdotes/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom'
+import About from './components/About'
+import Footer from './components/Footer'
+import CreateNew from './components/CreateNew'
+import Menu from './components/Menu'
+import AnecdoteList from './components/AnecdoteList'
+import Anecdote from './components/Anecdote'
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  const navigate = useNavigate()
+
+  const match = useMatch('/anecdotes/:id')
+  const anecdoteToShow = match ? anecdotes.find(a => a.id === Number(match.params.id)) : null
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+    setNotification(`A new anecdote "${anecdote.content}" created!`)
+    navigate('/')
+    setTimeout(() => {
+      setNotification('')
+    }, 5000)
+  }
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <div>
+        {notification}
+      </div>
+      <Routes>
+        <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdoteToShow}/>}/>
+        <Route path='/' element={<AnecdoteList anecdotes={anecdotes}/>}/>
+        <Route path='/create' element={<CreateNew addNew={addNew}/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState } from 'react'
+import { useField } from '../hooks'
+
+const CreateNew = (props) => {
+    const content = useField('text')
+    const author = useField('text')
+    const info = useField('text')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addNew({
+            content : content.value,
+            author : author.value,
+            info : info.value,
+            votes : 0
+        })
+    }
+
+    const handleReset = (e) => {
+        e.preventDefault()
+        content.onReset()
+        author.onReset()
+        info.onReset()
+    }
+    
+    return (
+        <div>
+            <h2>create a new anecdote</h2>
+            <form onSubmit={handleSubmit}>
+            <div>
+                content
+                <input name='content' {...content} />
+            </div>
+            <div>
+                author
+                <input name='author' {...author} />
+            </div>
+            <div>
+                url for more info
+                <input name='info' {...info} />
+            </div>
+            <button type='submit'>create</button>
+            <button onClick={handleReset}>reset</button>
+            </form>
+        </div>
+    )
+}
+
+export default CreateNew
