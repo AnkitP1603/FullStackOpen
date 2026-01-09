cb925648b6fcb6e501c98e2830400863881e57e8
Ankit Parkhi <ankitvilas.parkhi.cse23@itbhu.ac.in>
2026-01-09 16:34:42 +0530

    part-7.10

---
+++ part7/blogapp/frontend/src/components/Notification.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux"
+
+const Notification = () => {
+  const notification = useSelector((state) => state.notification)
+
+  const { message, type } = notification
+
+  if (!message) {
+    return null
+  }
+
+  const style = {
+    backgroundColor: 'lightgrey',
+    margin: '10px',
+    padding: '10px',
+    border: '2px solid',
+    borderColor: type === 'success' ? 'green' : 'red',
+    borderRadius: '5px',
+  }
+
+  return <div style={style}>{message}</div>
+}
+
+export default Notification
---
+++ part7/blogapp/frontend/src/reducers/notificationReducer.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState: {
+        message: '',
+        type: ''
+    },
+    reducers: {
+        setNotification(state, action) {
+            return action.payload
+        },
+        resetNotification(state, action) {
+            return {
+                message: '',
+                type: ''
+            }
+        }
+    }
+})
+
+let timeoutId = null
+
+export const showNotification = (message, type, timeout=5) => {
+    console.log('showing notification', message, type)
+    return dispatch => {
+        if(timeoutId) {
+            clearTimeout(timeoutId)
+        }
+        
+        dispatch(setNotification({message, type}))
+
+        timeoutId = setTimeout(() => {
+            dispatch(resetNotification())
+            timeoutId = null
+        }, timeout*1000 )
+    }
+}
+
+export const { setNotification, resetNotification } = notificationSlice.actions
+export default notificationSlice.reducer
